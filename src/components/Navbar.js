f423da9bc300364a63c9fa3ac0f50d1a60392067
crazynayan <nayan@crazyideas.co.in>
2020-07-29 13:42:57 +0530

    Template for Login, Home & Signup

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react"
+import Link from "react-router-dom/Link"
+import {AppBar, Toolbar, Button} from "@material-ui/core"
+
+class Navbar extends Component {
+  render() {
+    return (
+      <AppBar>
+        <Toolbar className={"nav-container"}>
+          <Button color={"inherit"} component={Link} to={"/login"}>Login</Button>
+          <Button color={"inherit"} component={Link} to={"/"}>Home</Button>
+          <Button color={"inherit"} component={Link} to={"/signup"}>Signup</Button>
+        </Toolbar>
+      </AppBar>
+    );
+  }
+}
+
+export default Navbar;
